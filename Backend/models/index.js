075d4633183244b1c28b7a88bd5a4f3b30c0309a
eregain <talktostevenson@gmail.com>
2024-11-26 05:12:40 -0800

    added mongodb for the backend

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
-
+const uri =
+  "mongodb+srv://talktostevenson:8N8xmd7hJeq0UBJw@de-damak.sue7t.mongodb.net/?retryWrites=true&w=majority&appName=De-damak";
 
 function main() {
-    mongoose.connect(uri).then(() => {
-        console.log("Succesfull")
-    
-    }).catch((err) => {
-        console.log("Error: ", err)
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("Succesfull");
     })
+    .catch((err) => {
+      console.log("Error: ", err);
+    });
 }
 
 module.exports = { main };
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # React-Inventory-Management-System
+
 Inventory Management System Built with React JS, Node JS, Express JS, MongoDB and Tailwind CSS.
 
-# [<span style="color: blue;">View Live Preview from here.</span>](https://inventory-management-rosy.vercel.app)
+# [<span style="color: blue;">View Live Preview from here.</span>]()
