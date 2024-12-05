d6d036e842484ef05978510a053086d34fafd144
SUNDAY STEVENSON EREMEH (EREGAIN) <38635276+eregain@users.noreply.github.com>
2024-12-05 14:39:57 -0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,6 @@ const cors = require("cors");
 const User = require("./models/users");
 const Product = require("./models/Product");
 
-
 const app = express();
 const PORT = 4000;
 main();
@@ -78,12 +77,10 @@ app.post("/api/register", (req, res) => {
   console.log("request: ", req.body);
 });
 
-
-app.get("/testget", async (req,res)=>{
-  const result = await Product.findOne({ _id: '6429979b2e5434138eda1564'})
-  res.json(result)
-
-})
+app.get("/testget", async (req, res) => {
+  const result = await Product.findOne({ _id: "6429979b2e5434138eda1564" });
+  res.json(result);
+});
 
 // Here we are listening to the server
 app.listen(PORT, () => {
